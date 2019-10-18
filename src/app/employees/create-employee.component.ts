fc6739bc0be2c55a861f1a1e7bdeaee5fd792fd9
John Sourvinos <johnsourvinos@hotmail.com>
2019-10-18 17:18:55 +0300

    On employee form, adds departments dropdown and photo preview

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { department } from './models/department';
 
 @Component({
     selector: 'app-create-employee',
@@ -9,12 +10,24 @@ import { NgForm } from '@angular/forms';
 
 export class CreateEmployeeComponent implements OnInit {
 
+    isPreviewPhoto: boolean = false
+
     fullName: string = ''
     email: string = ''
     gender: string = 'male'
     phone: string = ''
     contactPreference: string = 'email'
     isActive: boolean = true
+    department: string = '2'
+    dateOfBirth: Date
+    photoPath: string
+
+    departments: department[] = [
+        { id: 1, name: 'IT' },
+        { id: 2, name: 'Sales' },
+        { id: 3, name: 'Human resources' },
+        { id: 4, name: 'Marketing' }
+    ]
 
     constructor() { }
 
@@ -22,6 +35,10 @@ export class CreateEmployeeComponent implements OnInit {
 
     saveEmployee(empForm: NgForm) {
         console.log(empForm.value)
+    }
+
+    onShowPreview() {
+        this.isPreviewPhoto = !this.isPreviewPhoto
     }
 
 }
