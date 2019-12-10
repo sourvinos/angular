dd3559cc7d57c07196e8c3f058334db75cb5c4b1
John Sourvinos <johnsourvinos@hotmail.com>
2019-12-10 14:02:49 +0200

    localStorage

--- src/app/arrays/arrays.component.ts
+++ src/app/arrays/arrays.component.ts
@@ -1,8 +1,13 @@
-import { department } from './../employees/classes/model.department';
-import { Component, OnInit, ElementRef } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+
+export class Destination {
+	id: number
+	description: string
+}
 
 export class Fruit {
-	name: string
+	id: number
+	description: string
 }
 
 class Dummy {
@@ -20,28 +25,32 @@ class Dummy {
 
 export class ArraysComponent implements OnInit {
 
-	selectFruit(event: any, el: ElementRef) {
-		console.log(event, el)
-		document.getElementById('Apples').classList.add('selected')
-		document.getElementById('Mangos').classList.add('selected')
-	}
-
 	fruits: Fruit[] = [
-		{ name: 'Apples' },
-		{ name: 'Oranges' },
-		{ name: 'Figs' },
-		{ name: 'Mangos' },
-		{ name: 'Strawberries' },
+		{ id: 1, description: 'Apples' },
+		{ id: 2, description: 'Oranges' },
+		{ id: 3, description: 'Figs' },
+		{ id: 4, description: 'Mangos' },
+		{ id: 5, description: 'Strawberries' },
 	]
 
-	base: IDestination[] = [
+	destinations: Destination[] = [
+		{ id: 1, description: 'Paxos' },
+		{ id: 2, description: 'Corfu Town' },
+		{ id: 3, description: 'Blue Lagoon' },
+		{ id: 4, description: 'Erikousa' },
+	]
+
+	selectedFruits: Fruit[] = []
+	selectedDestinations: Destination[] = []
+
+	base: Destination[] = [
 		{ id: 4, description: 'AL' },
 		{ id: 6, description: 'AL' },
 		{ id: 1, description: 'BL' },
 		{ id: 2, description: 'BL' },
 		{ id: 5, description: 'BL' },
 		{ id: 3, description: 'PA' }]
-	baseFiltered: IDestination[]
+	baseFiltered: Destination[]
 	criteria = ['BL', 'AL']
 
 	transfers = [
@@ -63,9 +72,9 @@ export class ArraysComponent implements OnInit {
 	flatPeople: Dummy[] = []
 
 	ngOnInit() {
-		console.log(this.base)
+		// console.log(this.base)
 		this.baseFiltered = this.base.filter((x) => { return this.criteria.indexOf(x.description) !== -1 })
-		console.log(this.baseFiltered)
+		// console.log(this.baseFiltered)
 		for (var {
 			adults: n,
 			customer: {
@@ -77,10 +86,10 @@ export class ArraysComponent implements OnInit {
 					description: e
 				} }
 		} of this.transfers) {
-			console.log('Adults: ' + n + ', Customer: ' + f, ' Pickup point: ' + g + ' Route: ' + e)
+			// console.log('Adults: ' + n + ', Customer: ' + f, ' Pickup point: ' + g + ' Route: ' + e)
 			this.flatPeople.push({ adults: n, customer: f, pickupPoint: g, route: e })
 		}
-		console.log(this.flatPeople)
+		// console.log(this.flatPeople)
 	}
 
 	isGreaterThan(value: number, v: number) {
@@ -88,27 +97,58 @@ export class ArraysComponent implements OnInit {
 	}
 
 	onSaveToLocalStorage() {
-		let filteredFruits: Fruit[] = []
-		this.fruits.filter(fruit => {
-			if (fruit.name.includes('a'))
-				filteredFruits.push(fruit)
-		})
-
-		console.log('Filtered fruits', filteredFruits)
-		localStorage.setItem('fruits', JSON.stringify(filteredFruits))
-	}
-
-	onReadFromLocalStorage() {
-		console.log(JSON.parse(localStorage.getItem('fruits')))
+		let settings = {
+			"fruits": JSON.stringify(this.selectedFruits),
+			"destinations": JSON.stringify(this.selectedDestinations)
+		}
+		localStorage.setItem('settings', JSON.stringify(settings))
+		// localStorage.setItem('fruits', JSON.stringify(this.selectedFruits))
+		// localStorage.setItem('destinations', JSON.stringify(this.selectedDestinations))
 	}
 
 	onDisplayFruits() {
-		console.log(this.fruits)
+		// console.log(this.fruits)
+	}
+
+	/**
+	 * Adds/removes the clicked item to/from the 'selected...' array
+	 * 
+	 * @param item // The list item that was clicked
+	 * @param lookupArray // The array that holds the items with 'activeItem' class
+	 */
+	onToggleItem(item: any, lookupArray: string) {
+		var element = document.getElementById(item.description)
+		if (element.classList.contains('activeItem')) {
+			for (var i = 0; i < eval(lookupArray).length; i++) {
+				if (eval(lookupArray)[i] == item.description) {
+					eval(lookupArray).splice(i, 1)
+					i--
+					element.classList.remove('activeItem')
+					break
+				}
+			}
+		} else {
+			element.classList.add('activeItem')
+			eval(lookupArray).push(item.description)
+		}
+	}
+
+	onReadFromLocalStorage() {
+		let settings = JSON.parse(localStorage.getItem('settings'))
+
+		this.selectedFruits = JSON.parse(settings.fruits); this.readSummaryFromLocalStorage(this.selectedFruits, 'fruit')
+		this.selectedDestinations = JSON.parse(settings.destinations); this.readSummaryFromLocalStorage(this.selectedDestinations, 'destination')
+	}
+
+	readSummaryFromLocalStorage(array: any[], className: string) {
+		let elements = document.querySelectorAll('.' + className)
+		elements.forEach((element: HTMLElement) => {
+			let position = array.indexOf(element.innerHTML)
+			if (position != -1) {
+				document.getElementById(element.innerHTML).classList.add('activeItem')
+			}
+		})
 	}
 
 }
 
-export interface IDestination {
-	id: number
-	description: string
-}
